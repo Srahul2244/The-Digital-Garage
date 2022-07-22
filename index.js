d1781dcfbbe5d143e75146f1e8ef73ba03b7c272
Srahul2244 <99595875+Srahul2244@users.noreply.github.com>
2022-07-22 13:25:04 +0530

    added

---
+++ index.js
@@ -0,0 +1,53 @@
+var liveData = [
+    {
+      btn:"coming",
+      h: "Build Your Personal Brand Online",
+      p: "A tailored online presence can help attract new opportunities online, whether it’s a shot at a new role or new customers for your business. If you’re considering how to present yourself or your small business",
+      p1: "Career",
+      p2: " 22/07/22 at 11:00 AM Europe/London"
+    },
+    {
+        btn:"coming",
+        h: "Find Your Career Goals",
+        p: "Understanding what you enjoy in your job can help you take the next step in your career. Join us for a 60 minute session to explore what matters to you in your job. Try different exercises inspired by the Ikagi",
+        p1: "Career",
+        p2:" 25/07/22 at 14:00 PM Europe/London"
+    },
+    {
+        btn:"coming",
+        h: "Build a CV & Write a Cover Letter",
+        p: "What can you expect from this Google Digital Garage session? When applying for your first role, a well-built CV and cover letter can help your job application stand out to employers. Discover the steps required to",
+        p1: "28/07/22 at 14:00 PM Europe/London",
+        p2:"Jobseeker"
+    },
+    {
+        btn:"coming",
+        h: "Writing for Social Media",
+        p: "Engaging copy can help you say more with less when posting on your social media channels. If you manage a social media channel for your business and you’re looking at ways to sharpen your copy, join us for a",
+        p1: "27/07/22 at 16:00 PM Europe/London",
+        p2: "Student"
+    },
+    {
+        btn:"coming",
+        h: "Get Started with Analytics",
+        p: "Analytics can help you better understand your customers and support your digital marketing strategy. This course will help you to apply data-driven thinking to your business. Explore the questions your customer",
+        p1: "28/07/22 at 14:00 PM Europe/London",
+        p2:"Small & Medium Businesses"
+    },
+    {
+        btn:"coming",
+        h: "Build Your Personal Brand Online",
+        p: "Planning your presentation can help your delivery appear more natural and engaging. Learn how different storytelling techniques, presentation materials, and delivery styles can improve how you give presentations",
+        p1: "29/07/22 at 10:30 AM Europe/London",
+        p2: "Student"
+    },
+    {
+        btn:"coming",
+        h: "Meow 1",
+        p: "London",
+        p1: "12/12/22 at 01:00 AM Europe/London",
+        p2: "Jobseeker"
+    },
+   
+];
+localStorage.setItem("meData",JSON.stringify(liveData));
